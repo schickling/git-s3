2a414d2cb71d2fbf8080ef895789cc4b860582e3
Johannes Schickling <schickling.j@gmail.com>
2013-06-05 15:24:17 +0200

    Extended config file and setup aws

--- src/GitS3/Console/Application.php
+++ src/GitS3/Console/Application.php
@@ -2,23 +2,46 @@
 
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Yaml\Yaml;
+use Gitonomy\Git\Repository;
+use Aws\S3\S3Client;
 
 
 class Application extends BaseApplication
 {
 
 	private $config;
+	private $repository;
+	private $s3;
 	
 	public function __construct($name, $version)
 	{
 		parent::__construct($name, $version);
 
-		// load config
-		$this->config = Yaml::parse(__DIR__ . '/../../../config.yml');
+		// prepare s3 client
+		$config = Yaml::parse(__DIR__ . '/../../../config.yml');
+		$this->s3 = S3Client::factory($config);
+
+		// load repository
+		$this->repository = new Repository(__DIR__ . '/../../../');
 
 		// add available commands
 		$this->addCommands(array(
 			new Commands\DeployCommand()
 			));
+	}
+
+	public function getLastDeploy()
+	{
+		return file_get_contents(__DIR__ . '/../../../last-deploy.lock');
+	}
+
+	public function getRepository()
+	{
+		return $this->repository;
+	}
+
+	public function getS3()
+	{
+		return $this->s3;
 	}
 }
