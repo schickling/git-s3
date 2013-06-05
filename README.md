8be170169f4b487f643d27293e762e01f6851fee
Johannes Schickling <schickling.j@gmail.com>
2013-06-05 13:53:03 +0200

    Setup structure

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ git-s3
 ======
 
 Easy way to deploy your git repo to a AWS S3 bucket
+
+
+### Todo/Ideas
+* Commands
+    * init
+    * config
+    * set commit
+    * rollback
+    * log
---
+++ src/GitS3/Console/Command/DeployCommand.php
@@ -0,0 +1,17 @@
+<?php namespace GitS3\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DeployCommand extends Command {
+
+    protected function configure() {
+        $this->setName('deploy');
+        $this->setDescription('Deploy the current git repo');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $output->writeLn('Deployment goes here!');
+    }
+}
