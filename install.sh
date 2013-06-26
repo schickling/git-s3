9eb8b8defdd6005f69e976510a589a5fcf131fd9
Johannes Schickling <schickling.j@gmail.com>
2013-06-26 21:12:49 +0200

    Added installation script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# install composer dependencies
+echo "Installing dependencies..."
+
+if command -v composer > /dev/null; then
+	composer install --no-dev
+else
+	curl -sS https://getcomposer.org/installer | php
+	php composer.phar install --no-dev
+fi
+
+# run configuration
+php git-s3 config
+
+# finish
+echo ""
+echo "Good job."
+echo "Installation/Update was successful. Now type \"./git-s3 deploy\" to deploy your repository to your s3 bucket."
