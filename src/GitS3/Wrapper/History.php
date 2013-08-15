1b77597ae56e53ba00bac6afe5239239ba4bf312
Johannes Schickling <schickling.j@gmail.com>
2013-08-15 12:08:19 +0200

    Added history implementation

--- src/GitS3/Wrapper/History.php
+++ src/GitS3/Wrapper/History.php
@@ -2,5 +2,39 @@
 
 class History
 {
-	
+
+	private $hashes = array();
+	private $filePath;
+
+	public function __construct($filePath) {
+		$this->filePath = $filePath;
+		$this->load();
+	}
+
+	public function getHashes()
+	{
+		return $this->hashes;
+	}
+
+	public function getCurrentHash()
+	{
+		return $this->hashes[count($this->hashes) - 1];
+	}
+
+	public function addHash($hash)
+	{
+		array_push($this->hashes, $hash);
+	}
+
+	public function save()
+	{
+		$data = implode("\n", $this->hashes);
+		file_put_contents($this->filePath, $data);
+	}
+
+	private function load()
+	{
+		$data = file_get_contents($this->filePath);
+		$this->hashes = explode("\n", $data);
+	}
 }
--- tests/Wrapper/HistoryTest.php
+++ tests/Wrapper/HistoryTest.php
@@ -4,8 +4,18 @@ use GitS3\Wrapper\History;
 
 class HistoryTest extends PHPUnit_Framework_TestCase
 {
-	public function test()
+	public function testReadHistory()
 	{
-		
+		$history = new History(__DIR__ . '/resources/readHistory.lock');
+		$expectedHashes = array(
+			'20d4531193c0d78222152911ad932dc5a6caf320',
+			'20d4531193c0d78222152911ad932dc5a6caf321',
+			'20d4531193c0d78222152911ad932dc5a6caf322',
+			'20d4531193c0d78222152911ad932dc5a6caf323',
+			'20d4531193c0d78222152911ad932dc5a6caf324',
+			);
+
+		$this->assertEquals($expectedHashes, $history->getHashes());
+		$this->assertEquals('20d4531193c0d78222152911ad932dc5a6caf324', $history->getCurrentHash());
 	}
 }
