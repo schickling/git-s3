2b0419eb760ce7f209510fae55b49c0e900646ce
Johannes Schickling <schickling.j@gmail.com>
2013-08-26 16:11:27 +0200

    Fixed subfolder issue

--- src/GitS3/Wrapper/Diff.php
+++ src/GitS3/Wrapper/Diff.php
@@ -6,11 +6,20 @@ class Diff
 {
 	private $filesToUpload = array();
 	private $filesToDelete = array();
+	private $repository;
+	private $hashToCompare;
 	
 	public function __construct(Repository $repository, $hashToCompare)
 	{
-		$repository->diff($hashToCompare, array('name-status' => true));
-		$this->processOutput($repository->getOutput());
+		$this->repository = $repository;
+		$this->hashToCompare = $hashToCompare;
+		$this->prepare();
+	}
+
+	private function prepare()
+	{
+		$this->repository->diff($this->hashToCompare, array('name-status' => true));
+		$this->processOutput($this->repository->getOutput());
 	}
 
 	public function getFilesToUpload()
@@ -28,8 +37,15 @@ class Diff
 		$output = trim($output);
 		$lines = explode("\n", $output);
 
+		$repositoryRoot = trim($this->repository->run(array('rev-parse --show-toplevel'))->getOutput());
+		$repositorySubfolder = $this->repository->getDirectory();
+		$subFolder = str_replace($repositoryRoot . '/', '', $repositorySubfolder);
+
 		foreach ($lines as $line) {
 			list($action, $fileName) = explode("\t", $line);
+
+			// make file path relative to subdir
+			$fileName = str_replace($subFolder . '/', '', $fileName);
 
 			if ($action == 'D') {
 				array_push($this->filesToDelete, $fileName);
