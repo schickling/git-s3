fc914d889df7fc9a237d0a8c37d1b715474c81d5
Paddez <dave@paddez.com>
2015-02-04 17:10:13 +0000

    Adding IAM Support. Bucket will now check for valid Regex (From http://blogs.aws.amazon.com/security/post/Tx1XG3FX6VMU6O5/A-safer-way-to-distribute-AWS-credentials-to-EC2) If the Key and Secret fail the check, fall back to empty S3Client (Which will use Instance roles)
    If instance role does not exist, throw an accessKey error.

--- src/GitS3/Wrapper/Bucket.php
+++ src/GitS3/Wrapper/Bucket.php
@@ -3,7 +3,9 @@
 use Aws\S3\S3Client;
 use Aws\S3\Enum\CannedAcl;
 use Symfony\Component\Finder\SplFileInfo as File;
-
+use Aws\Common\Exception\InstanceProfileCredentialsException;
+use Aws\S3\Exception\InvalidAccessKeyIdException;
+use \Exception;
 class Bucket
 {
 
@@ -13,27 +15,50 @@ class Bucket
 	public function __construct($key, $secret, $name)
 	{
 		$this->name = $name;
-		$this->client = S3Client::factory(array(
-			'key'		=> $key,
-			'secret'	=> $secret,
+		
+		//Check for valid keys
+		$validKey = preg_match("/^(?<![A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])$/", $key);
+		$validSecret = preg_match("/^(?<![A-Za-z0-9\/+=])[A-Za-z0-9\/+=]{40}(?![A-Za-z0-9\/+=])$/", $secret);
+
+		//If keys are not valid, try a role.
+		if(!$validKey || !$validSecret)
+			$this->client = S3Client::factory(array());
+		else
+			$this->client = S3Client::factory(array(
+				'key'		=> $key,
+				'secret'	=> $secret,
 			));
 	}
 
 	public function upload(File $file)
 	{
-		$this->client->putObject(array(
-			'Bucket'		=> $this->name,
-			'Key'    		=> $file->getRelativePathname(),
-			'SourceFile' 	=> $file->getRealpath(),
-			'ACL'   		=> CannedAcl::PUBLIC_READ,
-			));
+		try
+		{
+			$this->client->putObject(array(
+				'Bucket'		=> $this->name,
+				'Key'    		=> $file->getRelativePathname(),
+				'SourceFile' 	=> $file->getRealpath(),
+				'ACL'   		=> CannedAcl::PUBLIC_READ,
+				));
+		}
+		catch(Exception $e)
+		{
+			throw new InvalidAccessKeyIdException("The AWS Access Key Id you provided does not exist in our records.");
+		}
 	}
 
 	public function delete($fileName)
 	{
-		$this->client->deleteObject(array(
-			'Bucket' => $this->name,
-			'Key'    => $fileName,
-			));
+		try
+		{
+			$this->client->deleteObject(array(
+				'Bucket' => $this->name,
+				'Key'    => $fileName,
+				));
+		}
+		catch(Exception $e)
+		{
+			throw new InvalidAccessKeyIdException("The AWS Access Key Id you provided does not exist in our records.");
+		}
 	}
 }
